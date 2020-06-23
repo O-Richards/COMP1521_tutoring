a62f0c2f82feb95a9ced7f40dca0ee653a8e610c
Oliver <oli.richards@gmail.com>
2020-06-23 16:56:41 +1000

    Added question stubs

---
+++ tut4_mips/q2.s
@@ -0,0 +1,19 @@
+# print the square of a number
+# #include <stdio.h>
+
+# int main(void) {
+#  int x, y;
+#  printf("Enter a number: ");
+#  scanf("%d", &x);
+#  y = x * x;
+#  printf("%d\n", y);
+#  return 0;// 
+# }
+
+
+
+############################
+# Register Definitions     #
+############################
+main:
+        jr      $ra
